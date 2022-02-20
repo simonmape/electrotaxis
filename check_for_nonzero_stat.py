32b4f63e0dd0b329d97de37e42699a91ad289fbd
Simon Martina-Perez <simonmartinaperez@gmail.com>
2022-02-20 14:53:30 +0000

    Fix 'directory' name in for loop

--- check_for_nonzero_stat.py
+++ check_for_nonzero_stat.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os
 
-for file in os.listdir(directory):
+for file in os.listdir(os.getcwd()):
     summary = np.loadtxt(file)
     print(file,'has',sum(np.all(arr, axis=1)),'nonzero rows')
