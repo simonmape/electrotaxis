fd140c32b5ee24c272f66a7b7f965c1d97aa46e0
Simon Martina-Perez <simonmartinaperez@gmail.com>
2022-02-20 14:54:00 +0000

    Changed name of array in for loop

--- check_for_nonzero_stat.py
+++ check_for_nonzero_stat.py
@@ -3,4 +3,4 @@ import os
 
 for file in os.listdir(os.getcwd()):
     summary = np.loadtxt(file)
-    print(file,'has',sum(np.all(arr, axis=1)),'nonzero rows')
+    print(file,'has',sum(np.all(summary, axis=1)),'nonzero rows')
