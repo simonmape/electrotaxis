8b7208b500883265d8200d2c2ef1f130be674649
Simon Martina-Perez <simonmartinaperez@gmail.com>
2022-02-27 15:25:15 +0000

    Initial commit

---
+++ profile_likelihood.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import re
+import os
+from tqdm import tqdm
+
+""""
+Parameter grid here is given by Gamma =  2
+U ranges from 0 to 20 in 25 increments
+v_sa ranges from 2 to 50 in 25 increments
+beta ranges from 0.05 to 2.05 in increments of 0.1
+cE ranges from 0.05 to 2.05 in incements of 0.1
+"""
+
+for sumstat in tqdm(os.listdir()):
+    #Read in parameter values used
+    param_list = re.split('G|C|b|w|u|',file.replace('.txt',''))[2:6]
+    param_list = [item.replace('_','.')[:-1] for item in param_list]
+    cE = float(param_list[0])
+    beta = float(param_list[1])
+    w = float(param_list[2])
+    u = float(param_list[3])
+
+    #Read the summary statistic
+    summary = np.loadtxt(sumstat)
+
+    #Compute log-likelihood for this sample
+    log_l = compute_log_likelihood(summary)
+
+    #Record log-likelihoood
