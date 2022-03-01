564084176085849963a4bddcfb410d7a3f561bae
Simon Martina-Perez <simonmartinaperez@gmail.com>
2022-03-01 09:44:37 +0000

    Fixed enumerate issues

--- make_profiles.py
+++ make_profiles.py
@@ -20,7 +20,7 @@ summary_stats = ['le', 'te', 'tbe', 'bs', 'bd', 'ls']
 parameters = ['cE', 'beta', 'w', 'u']
 parameter_ranges = [np.unique(pl[par].to_numpy()) for par in parameters]
 
-for i, summary_stat in enumerate(summary_stats):
+for i, summary_stat in tqdm(enumerate(summary_stats)):
     for j, parameter in enumerate(parameters):
         #Marginal profiles here
         profiles = [pl.loc[pl[parameter] == val].min() for val in parameter_ranges[j]]
@@ -29,13 +29,15 @@ for i, summary_stat in enumerate(summary_stats):
         np.savetxt(fName,marginal)
         for k, parameter2 in enumerate(parameters):
             #Pairwise profiles here
-            numPar1 = parameter_ranges[j].size[0]
-            numPar2 = parameter_ranges[k].size[0]
+            numPar1 = parameter_ranges[j].shape[0]
+            numPar2 = parameter_ranges[k].shape[0]
             pairwise = np.zeros((numPar1,numPar2))
 
-            for p1, par1 in parameter_ranges[j]:
-                for p2, par2 in parameter_ranges[k]:
-                    pairwise[p1,p2] = pl.loc[(pl[parameter] == par1) & (pl[parameter2]== par2)]
+            for p1 in range(numPar1):
+                for p2 in range(numPar2):
+                    par1 = parameter_ranges[j][p1]
+                    par2 = parameter_ranges[k][p2]
+                    pairwise[p1,p2] = pl.loc[(pl[parameter] == par1) & (pl[parameter2] == par2)].min()[summary_stat]
 
             fName = parameter+'_'+parameter2+'_'+summary_stat+'.txt'
-            np.savetxt(pairwise,marginal)
+            np.savetxt(fName,pairwise)
