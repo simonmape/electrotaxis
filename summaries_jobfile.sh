5c92bad539238a968e429b6ced6ea66c34f8ddf1
Simon Martina-Perez <simonmartinaperez@gmail.com>
2022-03-08 16:37:38 +0000

    Changed units for U

--- summaries_jobfile.sh
+++ summaries_jobfile.sh
@@ -12,3 +12,27 @@ source activate /data/math-multicellular-struct-devel/hert6124/fenicsproject
 cd ${DATA}/electrotaxis
 
 /data/math-multicellular-struct-devel/hert6124/fenicsproject/bin/python extract_summaries.py
+
+
+
+
+#!/bin/bash
+#SBATCH --job-name=elec
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=48
+#SBATCH --partition=short
+#SBATCH --output=outputs/summ%A_%a.out
+#SBATCH --error=outputs/summ%A_%a.out
+#SBATCH --array=1-441
+
+module purge
+module load Anaconda3
+source activate /data/math-multicellular-struct-devel/hert6124/fenicsproject
+
+cd ${SCRATCH}/electrotaxis
+
+#Get parameters for this simulation
+task_parameter=$(sed -n ${SLURM_ARRAY_TASK_ID}p params.txt)
+
+#Get the python dist in this conda environment to run the script
+/data/math-multicellular-struct-devel/hert6124/fenicsproject/bin/python summaries.py "${task_parameter}"
