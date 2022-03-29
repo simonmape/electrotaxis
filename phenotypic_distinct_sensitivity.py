80b422705c0905b85bc6ffa99de9cf11ce21c6c7
Simon Martina-Perez <simonmartinaperez@gmail.com>
2022-03-29 13:52:19 +0100

    fixed brackets for gradient

--- phenotypic_distinct_sensitivity.py
+++ phenotypic_distinct_sensitivity.py
@@ -204,7 +204,7 @@ class NSSolver:
 
         F_pder = inner(pder_new, z) * dx + (alpha / phicr) * inner((phi_old - phicr) * p_new, z) * dx - \
                dot(p_old, p_old) * alpha * inner(p_new, z) * dx + \
-               cE * (1 + delta_ph*inner(nabla_grad(phi_old),phi_old)/(1+inner(nabla_grad(phi_old),phi_old)) ) * inner(field, z) * dx - \
+               cE * (1 + delta_ph*inner(nabla_grad(phi_old),nabla_grad(phi_old))/(1+inner(nabla_grad(phi_old),nabla_grad(phi_old)))) * inner(field, z) * dx - \
                kappa * inner(nabla_grad(p_new), nabla_grad(z)) * dx - beta * inner(nabla_grad(phi_old), z) * dx
 
         F_pols = F_p + F_pder
